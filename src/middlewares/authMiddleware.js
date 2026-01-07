bc52ab7e1f0c2f3aa095ca88a20ee1276d856cec
FabinhoDeveloper <fabioezequiel555@gmail.com>
2026-01-07 15:14:44 -0300

    Middleware preparado para barrar requisicoes sem token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,17 +5,17 @@ export default function authMiddleware(req, res, next) {
     const token = authHeader && authHeader.split(" ")[1]
 
     if (token === null) {
-        console.log("Token não recebido")
+        return res.status(401).json({
+            error: true,
+            message: "Token não recebido!"
+        })
     }
 
     jwt.verify(token, process.env.SECRET, (err, user) => {
-        // if (err) return res.status(403).json({
-        //     error: true,
-        //     message: "Token não recebido, ou inválido!"
-        // })
-        if (err) {
-            console.log("Token inválido ou não recebido!")
-        }
+        if (err) return res.status(401).json({
+            error: true,
+            message: "Token não recebido, ou inválido!"
+        }) 
     })
 
     next()
