8f09958cf443ea72f4ab917013c75f9c0a50b637
FabinhoDeveloper <fabioezequiel555@gmail.com>
2026-01-15 16:08:29 -0300

    Notificacao enviada quando a viagem é confirmada

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,23 @@
 import admin from "firebase-admin";
-import serviceAccount from '../../queluz-app-firebase-adminsdk-fbsvc-b4bb26155b.json'
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+// Corrige __dirname em ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export default app
+const serviceAccountPath = path.join(
+  __dirname,
+  "../../queluz-app-firebase-adminsdk-fbsvc-b4bb26155b.json"
+);
+
+const serviceAccount = JSON.parse(
+  fs.readFileSync(serviceAccountPath, "utf-8")
+);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+export default admin;
